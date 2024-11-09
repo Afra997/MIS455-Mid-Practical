b5cd3bd69defd8e77446971089f40e6b21e9a5f6
Afra997 <157823071+Afra997@users.noreply.github.com>
2024-11-09 23:13:58 +0600

    fifth

---
+++ contact.js
@@ -0,0 +1,3 @@
+// Tahsina Islam Afra - ID: 2222755
+var contactMessage = "Welcome to the Contact Us Page!";
+console.log(contactMessage);
---
+++ products.js
@@ -0,0 +1,3 @@
+// Tahsina Islam Afra - ID: 2222755
+var productMessage = "Welcome to the Products Page!";
+console.log(productMessage);
